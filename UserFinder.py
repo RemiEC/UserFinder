63046cd3fa7a53325180c165a2e0ab343ed80058
Rémi GUILLON BONY <48432248+RemiEC@users.noreply.github.com>
2022-05-21 21:58:59 +0200

    Updated
    Progress Bar
    More verbose
    Exception management
    Keyboard Interrupt management
    Data saved despite exception

--- UserFinder.py
+++ UserFinder.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
+'''
+Written by Rémi Guillon Bony
+'''
+
 from ast import Try, arg
 from time import sleep
 import requests
 import argparse
+import sys
 
 wordlist = ""
 url = ""
@@ -17,10 +22,23 @@ output= ""
 
 def analysis():
     f = open(wordlist,"r")
-    for username in f:
-        send_request(username.strip())
-        sleep(0.001)
+    count = 0
+    file = f.readlines()
     f.close()
+    length = len(file)
+    for username in file:
+        send_request(username.strip())
+        count +=1
+        drawProgressBar(count/length)
+        sleep(0.001)
+    print("\nSearch completed ! \n")
+    
+
+def drawProgressBar(percent, barLen = 20):
+    # percent float from 0 to 1. 
+    sys.stdout.write("\r")
+    sys.stdout.write("[{:<{}}] {:.0f}%".format("=" * int(barLen * percent), barLen, percent * 100))
+    sys.stdout.flush()
 
 def send_request(username):
     payload = {username_variable:username, password_variable:"aaaaaaaaa"}
@@ -31,14 +49,14 @@ def send_request(username):
 
 def display():
 
-    BANNER = "    █████  █████                            ███████████  ███                 █████ \n"                
-    BANNER+= "   ░░███  ░░███                            ░░███░░░░░░█ ░░░                 ░░███ \n"                   
-    BANNER+= "    ░███   ░███   █████   ██████  ████████  ░███   █ ░  ████  ████████    ███████   ██████  ████████ \n" 
-    BANNER+= "    ░███   ░███  ███░░   ███░░███░░███░░███ ░███████   ░░███ ░░███░░███  ███░░███  ███░░███░░███░░███ \n" 
-    BANNER+= "    ░███   ░███ ░░█████ ░███████  ░███ ░░░  ░███░░░█    ░███  ░███ ░███ ░███ ░███ ░███████  ░███ ░░░ \n" 
-    BANNER+= "    ░███   ░███  ░░░░███░███░░░   ░███      ░███  ░     ░███  ░███ ░███ ░███ ░███ ░███░░░   ░███ \n" 
-    BANNER+= "    ░░████████   ██████ ░░██████  █████     █████       █████ ████ █████░░████████░░██████  █████ \n"    
-    BANNER+= "      ░░░░░░░░   ░░░░░░   ░░░░░░  ░░░░░     ░░░░░       ░░░░░ ░░░░ ░░░░░  ░░░░░░░░  ░░░░░░  ░░░░░ \n" 
+    BANNER = " █████  █████                            ███████████  ███                 █████ \n"                
+    BANNER+= "░░███  ░░███                            ░░███░░░░░░█ ░░░                 ░░███ \n"                   
+    BANNER+= " ░███   ░███   █████   ██████  ████████  ░███   █ ░  ████  ████████    ███████   ██████  ████████ \n" 
+    BANNER+= " ░███   ░███  ███░░   ███░░███░░███░░███ ░███████   ░░███ ░░███░░███  ███░░███  ███░░███░░███░░███ \n" 
+    BANNER+= " ░███   ░███ ░░█████ ░███████  ░███ ░░░  ░███░░░█    ░███  ░███ ░███ ░███ ░███ ░███████  ░███ ░░░ \n" 
+    BANNER+= " ░███   ░███  ░░░░███░███░░░   ░███      ░███  ░     ░███  ░███ ░███ ░███ ░███ ░███░░░   ░███ \n" 
+    BANNER+= " ░░████████   ██████ ░░██████  █████     █████       █████ ████ █████░░████████░░██████  █████ \n"    
+    BANNER+= "   ░░░░░░░░   ░░░░░░   ░░░░░░  ░░░░░     ░░░░░       ░░░░░ ░░░░ ░░░░░  ░░░░░░░░  ░░░░░░  ░░░░░ \n" 
     print("\n"+ BANNER) 
     print("             A situational tool for non-existing problems")
     print("             made by RemiEC \n \n \n")                                                
@@ -47,14 +65,14 @@ def display():
 
 def main():
     display()
-    parser = argparse.ArgumentParser(description='Find usernames based on differences in error messages')
+    parser = argparse.ArgumentParser(description='Find usernames based on error messages\' differences')
     parser.add_argument("url", help="Enter the target url")
     parser.add_argument("wordlist",  help="Wordlist of potential usernames")
     parser.add_argument("error_string", help="Text displayed when username OK but password KO")
     parser.add_argument("username_variable",  help="Name of username variable in POST request")
     parser.add_argument("password_variable", help="Name of password variable in POST request")
-    parser.add_argument("--cookie", metavar="", help="Cookie to be added, e.g. {\"cookie\":\"value\"}")
-    parser.add_argument("--output", metavar="filename", help="Output results to file")
+    parser.add_argument("-c", "--cookie", help="Cookie to be added")
+    parser.add_argument("-o", "--output", help="Output results to file")
     args = parser.parse_args()
     global wordlist 
     wordlist = args.wordlist
@@ -72,23 +90,57 @@ def main():
     output = args.output
 
     try:
-        print("Beginning search ... \n")
+        print("Beginning search ... ")
         analysis()
         if (len(OK_USERS) == 0):
             print("No username found :(")
         else:
-            print ("Valid usernames found ! \n")
-            if(output != None):
+            
+            if(output != ""):
+                print ("Valid usernames found ! I saved them in your output file \n")
                 f=open(output, "w")
                 for element in OK_USERS:
                     print(element)
                     f.write(element)
                 f.close()
             else:
+                print ("Valid usernames found !")
                 for element in OK_USERS:
                     print(element)
             
     except Exception:
-        print("\n Error :| ")
+        if (len(OK_USERS) == 0):
+            print("Error encountered, no username were found :(")
+        else:
+            print ("Error encountered, here are the results I managed to get (also saved in your output file) :  \n")
+            if(output != ""):
+                f=open(output, "w")
+                for element in OK_USERS:
+                    print(element)
+                    f.write(element)
+                f.close()
+            else:
+                print ("Error encountered, here are the results I managed to get :")
+                for element in OK_USERS:
+                    print(element)
+
+    except KeyboardInterrupt:
+
+        print("\n\nSeems like you got a bit impatient >:(")
+        if (len(OK_USERS) == 0):
+            print("I didn't have time to find anything juicy")
+        else:
+            print ("Here is what I managed to get anyway (also saved in your output file) :  \n")
+            if(output != ""):
+                f=open(output, "w")
+                for element in OK_USERS:
+                    print(element)
+                    f.write(element)
+                f.close()
+            else:
+                print ("Here is what I managed to get anyway")
+                for element in OK_USERS:
+                    print(element)
+        
 
 main()
